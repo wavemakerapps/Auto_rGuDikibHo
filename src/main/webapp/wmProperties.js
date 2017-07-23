c62e84c854e6d29060bb31523d1dcf34d46d9acd
root <root@61949b3d2f95>
2017-07-23 23:04:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_rGuDikibHo",
+  "homePage" : "Main",
+  "name" : "Auto_rGuDikibHo",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
